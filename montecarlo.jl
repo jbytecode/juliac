57dfa7372d6386f50404335818ecba05029e9b0a
jbytecode <mhsatman@gmail.com>
2025-05-23 12:22:18 +0300

    update montecarlo

--- montecarlo.jl
+++ montecarlo.jl
@@ -1,5 +1,3 @@
-module MonteCarloIntegrationExample 
-
 function normal(x::Float64)::Float64
 	return (1 / sqrt( 2 * pi)) * exp(-0.5 * x * x)
 end
@@ -12,10 +10,9 @@ function integrate(f::Function, a::Float64, b::Float64, n::Int64)::Float64
     return (b-a) *s / n    
 end 
 
-Base.@ccallable function main()::Cint
+function (@main)(args::Vector{String})::Cint
 	result = integrate(normal, -1.96, 1.96, 1000000)
 	println(Core.stdout, result)
 	return 0
 end
 
-end # End of module MonteCarloIntegrationExample
