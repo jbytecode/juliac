6bc3330d9d73bb5bfd83c677b0c494e3cf3c7175
jbytecode <mhsatman@gmail.com>
2025-07-01 08:50:27 +0300

    update download script and required files

--- download.sh
+++ download.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 rm -f juliac.jl
 rm -f juliac-buildscript.jl
 rm -f julia-config.jl
+rm -f juliac-trim-base.jl
+rm -f juliac-trim-stdlib.jl
+
 wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac.jl
 wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac-buildscript.jl
 wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/julia-config.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac-trim-base.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac-trim-stdlib.jl
+
 
