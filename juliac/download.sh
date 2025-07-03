f6befbd0afe2c23812e892f05d2f96d4da413b14
jbytecode <mhsatman@gmail.com>
2025-07-03 23:35:39 +0300

    change path of scripts

---
+++ juliac/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f juliac.jl
+rm -f juliac-buildscript.jl
+rm -f julia-config.jl
+rm -f juliac-trim-base.jl
+rm -f juliac-trim-stdlib.jl
+
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac/juliac.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac/juliac-buildscript.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/julia-config.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac/juliac-trim-base.jl
+wget -nc https://raw.githubusercontent.com/JuliaLang/julia/refs/heads/master/contrib/juliac/juliac-trim-stdlib.jl
+
+mv julia-config.jl ../julia-config.jl
+
